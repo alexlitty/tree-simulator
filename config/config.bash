0aaaaaa83cdec620e6fd7b8e4e3aed87d5e30110
Alexander Litty <alexander@litty.us>
2018-11-25 16:03:06 -0800

    Preventing multiple config.bash runs.

--- config/config.bash
+++ config/config.bash
@@ -1,10 +1,14 @@
 set -Eeuo pipefail
 
-export PATH_ROOT=$(cd $(dirname $0)/..; pwd)
+if [[ ! "${TREE_SIMULATOR_CONFIG:-}" ]]; then
+    export TREE_SIMULATOR_CONFIG=true 
 
-export PATH_BIN=${PATH_ROOT}/bin
-export PATH_BUILD=${PATH_ROOT}/build
-export PATH_CONFIG=${PATH_ROOT}/config
+    export PATH_ROOT=$(cd $(dirname $0)/..; pwd)
 
-export PATH_INCLUDE=${PATH_ROOT}/include
-export PATH_SOURCE=${PATH_ROOT}/src
+    export PATH_BIN=${PATH_ROOT}/bin
+    export PATH_BUILD=${PATH_ROOT}/build
+    export PATH_CONFIG=${PATH_ROOT}/config
+
+    export PATH_INCLUDE=${PATH_ROOT}/include
+    export PATH_SOURCE=${PATH_ROOT}/src
+fi
