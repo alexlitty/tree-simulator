f183e0027b6ffd4e78868b274676d520493f8ca6
Alexander Litty <alexander@litty.us>
2018-11-25 18:43:19 -0800

    Makefile flag and lib cleanup.

--- config/config.bash
+++ config/config.bash
@@ -6,9 +6,10 @@ if [[ ! "${TREE_SIMULATOR_CONFIG:-}" ]]; then
     export PATH_ROOT=$(cd $(dirname $0)/..; pwd)
 
     export PATH_BIN=${PATH_ROOT}/bin
-    export PATH_BUILD=${PATH_ROOT}/build
     export PATH_CONFIG=${PATH_ROOT}/config
 
+    export PATH_BUILD=${PATH_ROOT}/build
+    export PATH_LIB=${PATH_ROOT}/lib
     export PATH_INCLUDE=${PATH_ROOT}/include
     export PATH_SOURCE=${PATH_ROOT}/src
 fi
