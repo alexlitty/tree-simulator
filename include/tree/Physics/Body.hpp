7c0f947c8588c9f5d9d41e69f825ffadad409ba8
Alexander Litty <alexander@litty.us>
2018-12-24 03:16:05 -0800

    Adding more critical physical body methods.

--- include/tree/Physics/Body.hpp
+++ include/tree/Physics/Body.hpp
@@ -19,11 +19,43 @@ namespace tree
 
             ~Body();
 
+            void updateMass();
+
+            /**
+             * Owner.
+             */
             void setOwner(void *owner);
+            void* getOwner();
+
+            /**
+             * Position.
+             */
+            void setPosition(tree::Vector position);
+            tree::Vector getPosition() const;
+            tree::Vector getPixelPosition() const;
+
+            /**
+             * Angle.
+             */
+            void setAngle(tree::Angle angle);
+            tree::Angle getAngle() const;
+
+            /**
+             * Fixed rotation.
+             */
+            void setFixedRotation(bool fixedRotation);
+            bool getFixedRotation() const;
+
+            /**
+             * Miscellaneous setters.
+             */
             void setType(b2BodyType type);
             void setAngularDamping(float angularDamping);
-            void setFixedRotation(bool fixedRotation);
 
+            /**
+             * Miscellaneous getters.
+             */
+            float getMass() const;
             b2Body* getInternalBody();
         };
     }
--- src/Physics/Body.cpp
+++ src/Physics/Body.cpp
@@ -1,6 +1,8 @@
+#include <tree/Engine/Error.hpp>
 #include <tree/Physics/Body.hpp>
 #include <tree/Physics/World.hpp>
 
+// Constructor.
 tree::physics::Body::Body(bool dynamic, tree::Vector position)
 {
     b2BodyDef def;
@@ -15,16 +17,78 @@ tree::physics::Body::Body(bool dynamic, tree::Vector position)
     this->body = tree::physics::world.CreateBody(&def);
 }
 
+// Destructor.
 tree::physics::Body::~Body()
 {
     tree::physics::world.DestroyBody(this->body);
 }
 
+// Maintenance methods.
+void tree::physics::Body::updateMass()
+{
+    this->body->ResetMassData();
+}
+
+// Owner.
 void tree::physics::Body::setOwner(void *owner)
 {
     this->body->SetUserData(owner);
 }
 
+void* tree::physics::Body::getOwner()
+{
+    void *owner = this->body->GetUserData();
+
+    if (!owner) {
+        throw new tree::Error("Physics body is missing an owner!");
+    }
+
+    return owner;
+}
+
+// Position.
+void tree::physics::Body::setPosition(tree::Vector position)
+{
+    this->body->SetTransform(position, this->getAngle().radians());
+}
+
+tree::Vector tree::physics::Body::getPosition() const
+{
+    return this->body->GetWorldCenter();
+}
+
+tree::Vector tree::physics::Body::getPixelPosition() const
+{
+    return this->getPosition() * tree::PIXELS_PER_METER;
+}
+
+// Angle.
+void tree::physics::Body::setAngle(tree::Angle angle)
+{
+    bool originalFixedRotation = this->getFixedRotation();
+
+    this->setFixedRotation(false);
+    this->body->SetTransform(this->getPosition(), angle.radians());
+    this->setFixedRotation(originalFixedRotation);
+}
+
+tree::Angle tree::physics::Body::getAngle() const
+{
+    return tree::Angle::fromRadians(this->body->GetAngle());
+}
+
+// Fixed rotation.
+void tree::physics::Body::setFixedRotation(bool fixedRotation)
+{
+    this->body->SetFixedRotation(fixedRotation);
+}
+
+bool tree::physics::Body::getFixedRotation() const
+{
+    return this->body->IsFixedRotation();
+}
+
+// Miscellaneous setters.
 void tree::physics::Body::setType(b2BodyType type)
 {
     this->body->SetType(type);
@@ -35,9 +99,10 @@ void tree::physics::Body::setAngularDamping(float angularDamping)
     this->body->SetAngularDamping(angularDamping);
 }
 
-void tree::physics::Body::setFixedRotation(bool fixedRotation)
+// Miscellaneous getters.
+float tree::physics::Body::getMass() const
 {
-    this->body->SetFixedRotation(fixedRotation);
+    return this->body->GetMass();
 }
 
 b2Body* tree::physics::Body::getInternalBody()
